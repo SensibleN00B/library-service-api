553fb6f3a55333c4c57458af6ff2c14229df0a33
Vovas <vovaafon25@gmail.com>
2025-09-20 20:17:23 +0300

    fix: health check commit

--- payment/models.py
+++ payment/models.py
@@ -1,19 +1,19 @@
-from django.db import models
-
-
-class Payment(models.Model):
-    class Status(models.TextChoices):
-        pending = ("pending", "Pending")
-        paid = ("paid", "Paid")
-
-    class Type(models.TextChoices):
-        payment = ("payment", "Payment")
-        fine = ("fine", "Fine")
-
-    status = models.CharField(max_length=7, choices=Status, default=Status.pending)
-    type = models.CharField(max_length=7, choices=Type, default=Type.payment)
-    # borrowing = models.ForeignKey(Borrowing, related_name="payments",)
-    money_to_pay = models.PositiveIntegerField()
-    session_uri = models.CharField()
-    session_id = models.CharField()
+# from django.db import models
+#
+#
+# class Payment(models.Model):
+#     class Status(models.TextChoices):
+#         pending = ("pending", "Pending")
+#         paid = ("paid", "Paid")
+#
+#     class Type(models.TextChoices):
+#         payment = ("payment", "Payment")
+#         fine = ("fine", "Fine")
+#
+#     status = models.CharField(max_length=7, choices=Status, default=Status.pending)
+#     type = models.CharField(max_length=7, choices=Type, default=Type.payment)
+#     # borrowing = models.ForeignKey(Borrowing, related_name="payments",)
+#     money_to_pay = models.PositiveIntegerField()
+#     session_uri = models.CharField()
+#     session_id = models.CharField()
 
