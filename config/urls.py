4b28358fff806965eb8a105f513a9dbbba062d5d
Vovas <vovaafon25@gmail.com>
2025-09-23 00:14:45 +0300

    feat: add borrowing fine(overdue) feature

--- config/urls.py
+++ config/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
         include("borrowings.urls", namespace="borrowings"),
     ),
     path("api/library/", include("library.urls", namespace="library")),
-    path("api/stripe/", include("payment.urls", namespace="stripe")),
+    path("api/payment/", include("payment.urls", namespace="payment")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
--- payment/payment_service/stripe_service.py
+++ payment/payment_service/stripe_service.py
@@ -1,22 +1,33 @@
 import stripe
 from django.conf import settings
-from django.db import transaction
 from abc import ABC, abstractmethod
 
+from django.http import HttpRequest
 from django.urls import reverse
 
+from payment.models import Payment
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class PaymentService(ABC):
     @abstractmethod
-    def create_session(self, request, data):
+    def create_session(self, request: HttpRequest, data: dict):
+        pass
+
+    @abstractmethod
+    def create_payment(
+            self,
+            request: HttpRequest,
+            borrowing: "Borrowing",
+            money_to_pay: float,
+            payment_type=Payment.Type.payment
+    ):
         pass
 
 
 class StripePayment(PaymentService):
-    def create_session(self, request, data):
+    def create_session(self, request: HttpRequest, data: dict):
             session = stripe.checkout.Session.create(
                 payment_method_types=["card"],
                 mode="payment",
@@ -33,10 +44,39 @@ class StripePayment(PaymentService):
                     }
                 ],
                 success_url=request.build_absolute_uri(
-                    reverse("stripe:success", args=[data["payment"]])
+                    reverse("payment:success", args=[data["payment"]])
                 ),
                 cancel_url = request.build_absolute_uri(
-                    reverse("stripe:cancel", args=[data["payment"]])
+                    reverse("payment:cancel", args=[data["payment"]])
                 )
             )
             return session
+
+    def create_payment(
+            self,
+            request: HttpRequest,
+            borrowing: "Borrowing",
+            money_to_pay: float,
+            payment_type=None
+    ):
+        if payment_type is None:
+            payment_type = Payment.Type.payment
+        book_title = f"{borrowing.book.title} by {borrowing.book.author}"
+
+        payment = Payment.objects.create(
+            borrowing=borrowing,
+            money_to_pay=money_to_pay,
+            type=payment_type,
+        )
+
+        session = self.create_session(request, {
+            "book_title": book_title,
+            "money_to_pay": money_to_pay,
+            "payment": payment.id,
+        })
+
+        payment.session_id = session.id
+        payment.session_url = session.url
+        payment.save()
+
+        return payment
