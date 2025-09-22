a7be912d7a2bdadf6b52ad4fa17ad7198361c393
Serhii Basok <sergeybsk19@gmail.com>
2025-09-22 12:27:25 +0300

    feat: add action_serializers and action_permissions for BookViewSet

--- library/serializers.py
+++ library/serializers.py
@@ -11,6 +11,16 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
 
+    def validate_inventory(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Inventory cannot be negative.")
+        return value
+
+    def validate_daily_fee(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Daily fee cannot be negative.")
+        return value
+
 
 class BookListSerializer(serializers.ModelSerializer):
 
