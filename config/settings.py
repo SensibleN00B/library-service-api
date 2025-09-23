b34c5ade562e0e026d44fce72c4ba8dc63eb9799
Serhii Basok <sergeybsk19@gmail.com>
2025-09-23 16:47:11 +0300

    feat: schedule task to check expired Stripe sessions every minute

--- config/settings.py
+++ config/settings.py
@@ -193,6 +193,10 @@ CELERY_BEAT_SCHEDULE = {
         "task": "library.tasks.check_overdue_borrowings",
         "schedule": crontab(hour=9, minute=0),
     },
+    "check-expired-payments-every-minute": {
+        "task": "payment.tasks.check_expired_stripe_sessions",
+        "schedule": crontab(minute="*"),
+    },
 }
 
 STRIPE_SECRET_KEY = os.environ["STRIPE_SECRET_KEY"]
