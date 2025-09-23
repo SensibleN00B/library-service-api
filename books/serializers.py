a1f636cc6c2440817ce713278d283d42cf60b092
Hetmarum <kpxmedet@gmail.com>
2025-09-23 19:09:32 +0300

    feat: add picture field to BookSerializer fields

--- books/serializers.py
+++ books/serializers.py
@@ -7,7 +7,7 @@ class BookSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Book
-        fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
+        fields = ("id", "title", "author", "cover", "inventory", "daily_fee", "picture")
 
     @staticmethod
     def validate_inventory(value):
@@ -26,4 +26,4 @@ class BookListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Book
-        fields = ("id", "title", "author", "daily_fee")
+        fields = ("id", "title", "author", "daily_fee", "picture")
