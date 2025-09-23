fa99792ab3a4f0369ef31fc52e6ed57d0139e9aa
Bohdan Kolisnyk <bcolisnyk04@gmail.com>
2025-09-23 21:11:06 +0300

    feat: Enhance admin interfaces for Borrowing, Payment, and Book models: added custom list displays, search filters, inline editing, and admin actions."

--- payments/admin.py
+++ payments/admin.py
@@ -5,5 +5,31 @@ from payments.models import Payment
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "borrowing",
+        "user",
+        "book",
+        "type",
+        "status",
+        "money_to_pay",
+        "session_url",
+    )
+    list_filter = ("status", "type")
+    search_fields = ("borrowing__book__title", "borrowing__user__username")
+    actions = ["mark_as_paid"]
+
+    def user(self, obj):
+        return obj.borrowing.user
+
+    user.admin_order_field = "borrowing__user"
+    user.short_description = "User"
+
+    def book(self, obj):
+        return obj.borrowing.book.title
+
+    book.admin_order_field = "borrowing__book"
+    book.short_description = "Book"
+
     def get_queryset(self, request):
         return Payment.objects.select_related("borrowing__user", "borrowing")
