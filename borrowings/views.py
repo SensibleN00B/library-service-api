f97ece0d40c9a9b7d8aeeac2968e5b3d732197dd
Vovas <vovaafon25@gmail.com>
2025-09-23 23:27:49 +0300

    fix: decimal error(i dont understadn)

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,4 @@
-from decimal import Decimal
+from decimal import Decimal, InvalidOperation
 
 from django.conf import settings
 from django.core.exceptions import ValidationError
@@ -157,12 +157,13 @@ class BorrowingViewSet(
             )
 
         if overdue > 0:
+            raw_multiplier = str(settings.FINE_MULTIPLIER)
+            try:
+                FINE_MULTIPLIER = Decimal(raw_multiplier)
+            except InvalidOperation:
+                raise ValueError(f"Invalid FINE_MULTIPLIER: {raw_multiplier}")
             stripe_payment = StripePayment()
-            money_to_pay = (
-                overdue
-                * borrowing.book.daily_fee
-                * Decimal(str(settings.FINE_MULTIPLIER))
-            )
+            money_to_pay = overdue * borrowing.book.daily_fee * FINE_MULTIPLIER
 
             with transaction.atomic():
                 payment = stripe_payment.create_payment(
