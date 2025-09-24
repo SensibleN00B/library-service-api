eab761ee70c986ddfb0f9ae22868d7352cb3e4fa
Serhii Basok <sergeybsk19@gmail.com>
2025-09-24 10:06:08 +0300

    feat: add EmptySerializer to payments serializers

--- payments/serializers.py
+++ payments/serializers.py
@@ -40,3 +40,6 @@ class PaymentDetailSerializer(serializers.ModelSerializer):
             "session_url",
             "session_id",
         )
+
+class EmptySerializer(serializers.Serializer):
+    pass
